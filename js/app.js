d288791cd414973f9b2d75caac866838001eb40f
lowercasesea <ceunger91@gmail.com>
2020-05-01 23:08:21 -0400

    added shuffle function

--- js/app.js
+++ js/app.js
@@ -11,9 +11,27 @@ const global = {
 //////////////////////////
 
 const func = {
-
+    // shuffle cards
+    shuffle(arr) {
+        // Random Number Function
+        const randomNumber = (arrLen) => Math.floor(Math.random() * arrLen);
+        // grab the index of the last array element
+        let lastIndex = arr.length - 1;
+        // initialize a temporary variable to hold the numbers as we swap
+        let temp = 0;
+        // loop through the array backwards from the end, ignoring index 0
+        for (let i = lastIndex; i > 0; i--) {
+            // temporarily store the current index's value
+            temp = arr[i];
+            // find a random index between the current index and index 1
+            randomIndex = randomNumber(i)
+            // set the current index's value to the value of the random index
+            arr[i] = arr[randomIndex];
+            // set that random index's value to the current index's value, which we stored in temp
+            arr[randomIndex] = temp;
+        }
+    }
 }
-
 //////////////////////////
 // Event Handlers
 //////////////////////////
