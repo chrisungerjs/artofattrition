efd613dc13892f5e9e069588363650860bdd6e72
lowercasesea <ceunger91@gmail.com>
2020-05-02 15:41:54 -0400

    initial css in place

--- js/app.js
+++ js/app.js
@@ -3,14 +3,64 @@
 //////////////////////////
 
 const global = {
-
+    cardsInShop: [],
+    cardsInPool: [],
+    backgroundClasses: [
+        'orc-fortress',
+        'troll-city',
+        'castle',
+        'guincho',
+        'flying-fortress',
+        'imeall',
+        'hidden-realm',
+    ],
+    
 }
 
 //////////////////////////
-// Functions
+// Classes
+//////////////////////////
+
+class Player {
+    constructor () {
+        this.name = 0;
+        this.health = 20;
+        this.currentTier = 1;
+        this.cardsInPlay = [];
+    }
+}
+
+class Card {
+    constructor (name, power, health, img) {
+        this.name = name;
+        this.power = power;
+        this.health = health;
+        this.img = img;
+    }
+}
+
+//////////////////////////
+// Global Functions
 //////////////////////////
 
 const func = {
+    // press start to begin
+    startGame() {
+        this.generatePool();
+        this.shuffle(global.cardsInPool);
+        this.makeCardElements(); // for both players
+    },
+    // generate the starting card pool
+    generatePool() {
+
+    },
+    // generate card DOM elements
+    makeCardElements(cardArray) {    
+        for (card in cardArray) {
+            $('<div>').addClass('card');
+            // need to wireframe/design the dom cards
+        }
+    },
     // shuffle cards
     shuffle(arr) {
         // Random Number Function
@@ -30,8 +80,27 @@ const func = {
             // set that random index's value to the current index's value, which we stored in temp
             arr[randomIndex] = temp;
         }
-    }
+    },
+    buyRound(qty) {
+        // shuffle the card pool
+        func.shuffle(global.cardsInPool);
+        // add the appropriate amount of cards to the shop
+        for (let i = 0; i < qty; i++) {
+            global.cardsInShop.push(global.cardsInPool.pop());
+        }
+        // loop that makes dom elements for the cards
+        // make listeners too
+
+
+        // make your creatures clickable so you can sell them if you want
+        // 
+    },
+
+    combat() {
+    },
+
 }
+
 //////////////////////////
 // Event Handlers
 //////////////////////////
@@ -43,4 +112,3 @@ const handle = {
 //////////////////////////
 // App Logic
 //////////////////////////
-
